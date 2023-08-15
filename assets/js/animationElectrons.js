4a548ea060519ad595a151b1b250d7492f504a76
anastar5858 <anas.tarrab58@gmail.com>
2023-08-15 01:29:15 +0100

    all linkage done now electron movement time

--- assets/js/animationElectrons.js
+++ assets/js/animationElectrons.js
@@ -1,4 +1,5 @@
 export const configureCanvasDimensions = (canvas, btn) => {
+    canvas.style.zIndex = 1;
     const body = document.getElementsByClassName('body')[0];
     const bodyArea = body.getBoundingClientRect();
     canvas.style.position = 'absolute';
@@ -46,6 +47,11 @@ const animateRight = (ctx, start, animationStep, y, rightX, calcContainerArea) =
 
 const animateUp = (ctx, x, animationStep, start, upY, calcContainerArea) => {
     if (Math.floor(start) <= Math.floor(upY)) {
+        // move left to mid poing of calc top edge
+        const leftX = calcContainerArea.left + ((calcContainerArea.right - calcContainerArea.left) / 2);
+        const distanceX = x - leftX
+        animationStep = Math.round((distanceX / 30) * 100) / 100;
+        requestAnimationFrame(() => animateLeft(ctx, x, animationStep, start, leftX, calcContainerArea))
         return
     }
     ctx.beginPath();
@@ -55,4 +61,39 @@ const animateUp = (ctx, x, animationStep, start, upY, calcContainerArea) => {
     ctx.closePath();
     start -= animationStep
     requestAnimationFrame(() => animateUp(ctx, x, animationStep, start, upY, calcContainerArea) )
+}
+const animateLeft = (ctx, start, animationStep, y, leftX, calcContainerArea) => {
+    if (Math.floor(start) <= Math.floor(leftX)) {
+        // now link to display
+        const displayArea = document.getElementById('display-area');
+        const displayAreaBox = displayArea.getBoundingClientRect();
+        // now last lines animation
+        const bottomY = displayAreaBox.top - 20;
+        const distanceY = y - bottomY
+        animationStep = Math.round((distanceY / 30) * 100) / 100;
+        requestAnimationFrame(() => animateBottom(ctx, start, animationStep, y, bottomY, calcContainerArea))
+
+        return
+    }
+    ctx.beginPath();
+    ctx.lineTo(start, y);
+    ctx.lineTo(start - animationStep, y)
+    ctx.stroke();
+    ctx.closePath();
+    start -= animationStep
+    requestAnimationFrame(() => animateLeft(ctx, start, animationStep, y, leftX, calcContainerArea))
+}
+
+const animateBottom = (ctx, x, animationStep, start, bottomY, calcContainerArea) => {
+    if (Math.floor(start) >= Math.floor(bottomY)) {
+        // time to introduce the electron
+        return
+    }
+    ctx.beginPath();
+    ctx.lineTo(x, start);
+    ctx.lineTo(x, start - animationStep)
+    ctx.stroke();
+    ctx.closePath();
+    start -= animationStep
+    requestAnimationFrame(() => animateBottom(ctx, x, animationStep, start, bottomY, calcContainerArea))
 }
